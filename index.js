b8d2e8519bb00548cf03e6d92337a9c30efdce5e
leonbyrdjr <leonbyrdjr@Leons-MacBook-Pro.local>
2022-06-24 16:06:17 -0500

    commit tree cb8f11b08dd4fe1dcea20a6c93d61bacd7c6831d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+
+function findMatching(drivers, name){
+    // console.log(name)
+    // const notMatching = [];
+    // for(let driver of drivers){
+    //     if(driver.toLowerCase() === name.toLowerCase()){
+    //     notMatching.push(driver)
+    //     }
+    // }
+    // return notMatching;
+    let newDrivers = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return newDrivers;
+}
+
+
+function fuzzyMatch(drivers, name){
+    console.log(name)
+    let newDrivers = drivers.filter(driver => driver.charAt(0,1) === name.charAt(0,1))
+    return newDrivers;
+}
+function matchName(drivers, string){
+    console.log(drivers)
+    console.log(string)
+    let newDrivers = drivers.filter(driver => driver.name === string)
+    return newDrivers;
+}
+
+
